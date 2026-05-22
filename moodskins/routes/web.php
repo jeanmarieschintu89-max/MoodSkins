aecd8735b4b12ece41b953f37a1984479535003c
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-22 03:32:40 +0200

    Use explicit MoodSkins controller routes

--- moodskins/routes/web.php
+++ moodskins/routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use Azuriom\Plugin\Moodskins\Controllers\MoodSkinsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/moodskins/ping', function () {
+    return response('MoodSkins OK', 200);
+})->name('moodskins.ping');
+
 Route::prefix('moodskins')->group(function () {
-    Route::get('/data/{name}', 'MoodSkinsController@data')->name('moodskins.data');
-    Route::get('/avatar/{name}', 'MoodSkinsController@avatar')->name('moodskins.avatar');
-    Route::get('/skin/{name}', 'MoodSkinsController@skin')->name('moodskins.skin');
-    Route::get('/head/{name}', 'MoodSkinsController@head')->name('moodskins.head');
+    Route::get('/data/{name}', [MoodSkinsController::class, 'data'])->name('moodskins.data');
+    Route::get('/avatar/{name}', [MoodSkinsController::class, 'avatar'])->name('moodskins.avatar');
+    Route::get('/skin/{name}', [MoodSkinsController::class, 'skin'])->name('moodskins.skin');
+    Route::get('/head/{name}', [MoodSkinsController::class, 'head'])->name('moodskins.head');
 });

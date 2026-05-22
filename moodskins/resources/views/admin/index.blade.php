4e66784ac3ffdd4a1d7f64662274613e096f5630
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-22 03:09:06 +0200

    Add MoodSkins admin view

---
+++ moodskins/resources/views/admin/index.blade.php
@@ -0,0 +1,18 @@
+@extends('admin.layouts.admin')
+
+@section('title', 'MoodSkins')
+
+@section('content')
+    <div class="card shadow mb-4">
+        <div class="card-header">
+            <h6 class="m-0 font-weight-bold text-primary">MoodSkins</h6>
+        </div>
+        <div class="card-body">
+            <p>MoodSkins est actif.</p>
+            <p>Utilise ces liens pour tester un joueur Bedrock :</p>
+            <pre>/moodskins/data/.Moody6034
+/moodskins/avatar/.Moody6034
+/moodskins/skin/.Moody6034</pre>
+        </div>
+    </div>
+@endsection

eb85ee98c9865ded4841171d068dfe6961d4440f
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-22 03:08:47 +0200

    Add installable service provider

---
+++ moodskins/src/Providers/MoodskinsServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Azuriom\Plugin\Moodskins\Providers;
+
+use Azuriom\Extensions\Plugin\BasePluginServiceProvider;
+use Azuriom\Plugin\Moodskins\Services\GeyserSkinService;
+
+class MoodskinsServiceProvider extends BasePluginServiceProvider
+{
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../../config/moodskins.php', 'moodskins');
+
+        $this->app->singleton(GeyserSkinService::class, function () {
+            return new GeyserSkinService();
+        });
+    }
+
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../../config/moodskins.php' => config_path('moodskins.php'),
+        ], 'moodskins-config');
+    }
+}

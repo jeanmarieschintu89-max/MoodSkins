814625a62840cf46a58bcd267f6bc341982a06e0
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-22 03:08:05 +0200

    Add installable MoodSkins config

---
+++ moodskins/config/moodskins.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'bedrock_prefix' => '.',
+    'geyser_api_base' => 'https://api.geysermc.org/v2',
+    'cache_minutes' => 1440,
+    'fallback_avatar' => '/assets/img/avatar.png',
+];

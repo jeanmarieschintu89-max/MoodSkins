14d6ffcadc74fc515b7eb889e07bcc8273c81c33
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-22 03:32:29 +0200

    Use Laravel route loader for MoodSkins

--- moodskins/src/Providers/MoodskinsServiceProvider.php
+++ moodskins/src/Providers/MoodskinsServiceProvider.php
@@ -4,7 +4,6 @@ namespace Azuriom\Plugin\Moodskins\Providers;
 
 use Azuriom\Extensions\Plugin\BasePluginServiceProvider;
 use Azuriom\Plugin\Moodskins\Services\GeyserSkinService;
-use Illuminate\Support\Facades\Route;
 
 class MoodskinsServiceProvider extends BasePluginServiceProvider
 {
@@ -21,17 +20,10 @@ class MoodskinsServiceProvider extends BasePluginServiceProvider
     {
         $this->loadViews();
         $this->loadTranslations();
-        $this->loadMoodSkinsRoutes();
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
 
         $this->publishes([
             __DIR__ . '/../../config/moodskins.php' => config_path('moodskins.php'),
         ], 'moodskins-config');
-    }
-
-    private function loadMoodSkinsRoutes(): void
-    {
-        Route::middleware('web')
-            ->namespace('Azuriom\\Plugin\\Moodskins\\Controllers')
-            ->group(__DIR__ . '/../../routes/web.php');
     }
 }
